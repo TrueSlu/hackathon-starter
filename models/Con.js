63376e8bd6d2fac7b6f8efebcc5252527f4e77d1
Steven Lu <slu.true@gmail.com>
2019-01-17 17:59:58 -0800

    asdf

--- models/Con.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const conSchema = new mongoose.Schema({
-	text: String,
-}, { timestamps: true });
-
-const Con = mongoose.model('Con', conSchema);
-
-module.exports = Con;
